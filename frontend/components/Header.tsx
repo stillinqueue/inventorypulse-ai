02e7546ac2cf08e396efe058cd5015fb0d3d1925
stillinqueue <stillinqueue25@gmail.com>
2026-07-07 10:16:24 +0200

    Delete frontend directory

--- frontend/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-export default function Header({ title, subtitle }: { title: string; subtitle: string }) {
-  return (
-    <div className="header">
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
-    </div>
-  );
-}
